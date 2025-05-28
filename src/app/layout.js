c46dea675210439c300e9a5f7ba93b55c14fb62e
calestix <calestixinfotech@gmail.com>
2025-05-28 14:37:26 +0530

    r2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import ClientLayout from "./ClientLayout";
 import "./globals.css";
+import {AuthProvider} from "./context/auth.context"
+
 
 export const metadata = {
   title: "FORGE T&C EST",
@@ -13,7 +15,9 @@ export default function RootLayout({ children }) {
       <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
       </head>
       <body>
+        <AuthProvider>
         <ClientLayout>{children}</ClientLayout>
+        </AuthProvider>
       </body>
     </html>
   );
